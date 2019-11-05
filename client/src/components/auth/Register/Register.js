9ac725f6f51f009c9afd1094cb75431477f9e6f9
asathiya007 <akshay.sathiya@gmail.com>
2019-11-04 23:21:24 -0500

    flushed out errors on the landing page UI

--- client/src/components/auth/Register/Register.js
+++ client/src/components/auth/Register/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Register = () => {
     return (
+        // use tachyons register form component!
         <div>
             
         </div>
--- client/src/components/layout/Video/Video.js
+++ client/src/components/layout/Video/Video.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import vid from "../../../vid/earth.mp4";
+import "./Video.css";
 
 const Video = () => {
     return (
-        <video src={vid} autoPlay="autoplay" loop style={{
-            width: "100vw",
-            position: "fixed",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            zIndex: -1
-        }}></video>
+        <video src={vid} autoPlay="autoplay" loop className="video"></video>
     )
 }
 
