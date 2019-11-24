80c300a6fd37d631770242de12b1dffcfa4177ad
asathiya007 <akshay.sathiya@gmail.com>
2019-11-24 16:06:00 -0500

    created api routes for user profile details

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get("/test", (req, res) => res.json({msg: "api running"}));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/messages", require("./routes/api/messages"));
+app.use("/api/profile", require("./routes/api/profile")); 
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
