f29611091fd40af089b957890aadb6c050792299
asathiya007 <akshay.sathiya@gmail.com>
2019-11-27 13:01:59 -0500

    built the create profile action, phone number verification

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import {
     GET_PROFILE, 
     PROFILE_ERROR
 } from "./types";
+import {produceAlert} from "../actions/alert";
 
 // get current user's profile 
 export const getCurrentProfile = () => async dispatch => {
@@ -16,6 +17,40 @@ export const getCurrentProfile = () => async dispatch => {
         dispatch({
             type: PROFILE_ERROR, 
             payload: {msg: err.response.statusText, status: err.response.status}
+        }); 
+    }
+}
+
+// create or update current user's profile 
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        const res = await axios.post("/api/profile", formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        }); 
+
+        dispatch(produceAlert(edit ? "profile updated" : "profile created", "success"));
+
+        if (!edit) {
+            history.push("/dashboard");
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if (errors) {
+            errors.forEach(error => dispatch(produceAlert(error.msg, "danger")));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
         }); 
     }
 }
