9a163f71cc3ad8513503740d912391055be69dcf
asathiya007 <akshay.sathiya@gmail.com>
2020-01-01 23:20:40 -0800

    added triangle exclamation icon to not found page

--- client/src/components/layout/NotFound/NotFound.js
+++ client/src/components/layout/NotFound/NotFound.js
@@ -4,7 +4,8 @@ const NotFound = () => {
     return (
         <div className="top-space tc background-dark br4 w-80 pb5">
             <h1 className="white-text x-large">
-                Page Not Found
+                <i class="fas fa-exclamation-triangle white-hover-1"></i>
+                {' '}Page Not Found
             </h1>
             <p className="f2">Sorry, this page doesn't exist!</p>
         </div>
